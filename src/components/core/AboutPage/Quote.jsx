1458914630134d6db7fd235c890e492062e9b616
AmalenduGuru01 <aguru_be21@thapar.edu>
2024-04-22 19:42:32 +0530

    some changes

--- src/components/core/AboutPage/Quote.jsx
+++ src/components/core/AboutPage/Quote.jsx
@@ -8,7 +8,7 @@ const Quote = () => {
         innovative platform <HighlightText text={"combines technology"} />,{" "}
         <span className="bg-gradient-to-b from-[#FF512F] to-[#F09819] text-transparent bg-clip-text font-bold">
             {" "}
-            expertise
+            XpertiseX
         </span>
         , and community to create an
         <span className="bg-gradient-to-b from-[#E65C00] to-[#F9D423] text-transparent bg-clip-text font-bold">
